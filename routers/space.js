2d7f0f2721b1ba994f92bad2e7a31d008019ead4
emerymastering <emery@mastering.lt>
2022-09-23 17:52:13 +0200

    feat 6 done. created endpoint for updating profile

--- routers/space.js
+++ routers/space.js
@@ -87,4 +87,20 @@ router.post("/:id/stories", auth, async (req, res) => {
   return res.status(201).send({ message: "Story created", story });
 });
 
+// PATCH - update space details
+router.patch("/:id", auth, async (req, res) => {
+  const space = await Space.findByPk(req.params.id);
+  if (!space.userId === req.user.id) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to update this space" });
+  }
+
+  const { title, description, backgroundColor, color } = req.body;
+
+  await space.update({ title, description, backgroundColor, color });
+
+  return res.status(200).send({ space });
+});
+
 module.exports = router;
